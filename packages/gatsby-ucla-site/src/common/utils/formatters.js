2830d37df69ac5eaba184a013e1366a63d80e395
Josh Maas-Howard <j.maas.howard@gmail.com>
2020-12-14 17:08:10 -0500

    legend edge case

--- packages/gatsby-ucla-site/src/common/utils/formatters.js
+++ packages/gatsby-ucla-site/src/common/utils/formatters.js
@@ -3,6 +3,7 @@ import { METRIC_FORMATTERS } from "../constants"
 export const formatMetricValue = (value, metric) => {
   const format = METRIC_FORMATTERS[metric] || ((d) => d)
   if (typeof value !== "number" || !Number.isFinite(value)) {
+    // fixes #55
     return "N/A"
   }
   return format(value)
