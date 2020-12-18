d05072ca45ceb3303209ad3e616facb6dcd0921b
Josh Maas-Howard <j.maas.howard@gmail.com>
2020-12-17 22:19:23 -0500

    #45 - filter statewide facilities out for homepage

--- packages/gatsby-ucla-site/src/components/home/HomeTable.js
+++ packages/gatsby-ucla-site/src/components/home/HomeTable.js
@@ -199,7 +199,7 @@ const HomeTable = ({ title, note, classes, ...props }) => {
         <MetricSelectionTitle title={title} />
         <Table
           className={classes.table}
-          data={data}
+          data={data.filter((d) => d.name !== "Statewide")}
           columns={columns}
           options={options}
           sortColumn={metric}
