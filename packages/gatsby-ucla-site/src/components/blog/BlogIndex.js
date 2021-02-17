58d195c0c9b54ae2c4a8f21ebe7433d035f5a01e
James Minton <james-minton@github.com>
2021-02-17 14:56:30 -0500

    Activate blog menu

--- packages/gatsby-ucla-site/src/components/blog/BlogIndex.js
+++ packages/gatsby-ucla-site/src/components/blog/BlogIndex.js
@@ -188,10 +188,8 @@ const BlogIndex = () => {
   const classes = useStyles()
   return (
     <Layout className={classes.layout}>
+      
       <div className={classes.featuredSection}>
-        <h2 className={classes.sectionTitle}>coming soon</h2>
-      </div>
-      {/* <div className={classes.featuredSection}>
         <h2 className={classes.sectionTitle}>featured post</h2>
         <BlogPost post={featuredPost} isFeatured={true} />
       </div>
@@ -201,7 +199,7 @@ const BlogIndex = () => {
         {posts.map((p, idx) => (
           <BlogPost post={p} key={p.frontmatter.path} />
         ))}
-      </div> */}
+      </div> 
     </Layout>
   )
 }
