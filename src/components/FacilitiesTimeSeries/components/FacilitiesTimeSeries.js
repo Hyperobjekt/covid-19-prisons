a93c85dd090345d73c6f5f3f08d1ee05dae715c5
Josh Maas-Howard <j.maas.howard@gmail.com>
2021-06-12 13:32:04 -0400

    add parsedFacilityMap to cache facility data that has already been parsed

--- src/components/FacilitiesTimeSeries/components/FacilitiesTimeSeries.js
+++ src/components/FacilitiesTimeSeries/components/FacilitiesTimeSeries.js
@@ -2,11 +2,15 @@ import React from "react";
 import { getLang } from "../../../common/utils/i18n";
 import { Block } from "@hyperobjekt/material-ui-website";
 import FacilitySelect from "./FacilitySelect";
+import useTimeSeriesData from "../../../common/hooks/use-time-series-data";
 
 const FacilitiesTimeSeries = ({ ...props }) => {
+  const data = useTimeSeriesData();
+  const disp = Object.keys(data);
   return (
     <Block>
       <FacilitySelect />
+      {disp.join(", ")}
     </Block>
   );
 };
