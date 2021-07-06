579ad650f4fee26b78c66e1cf0799273c7e86215
lane <laneolson@gmail.com>
2021-07-06 11:41:55 -0700

    consistent styling for tooltips

--- src/components/IconWithTooltip.js
+++ src/components/IconWithTooltip.js
@@ -1,15 +1,15 @@
-import React from "react"
-import PropTypes from "prop-types"
-import InfoIcon from "../../content/assets/info-icon.svg"
+import React from "react";
+import PropTypes from "prop-types";
+import InfoIcon from "../../content/assets/info-icon.svg";
 import {
   Box,
   ButtonBase,
   makeStyles,
   Tooltip,
   Typography,
-} from "@material-ui/core"
-import { sansSerifyTypography } from "../gatsby-theme-hypercore/theme"
-import clsx from "clsx"
+} from "@material-ui/core";
+import { sansSerifyTypography } from "../gatsby-theme-hypercore/theme";
+import clsx from "clsx";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -36,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
   title: {
     ...sansSerifyTypography,
     display: "block",
-    fontSize: theme.typography.pxToRem(14),
+    fontSize: 14,
     fontWeight: 700,
     marginTop: 0,
     paddingBottom: theme.spacing(1),
@@ -45,12 +45,13 @@ const useStyles = makeStyles((theme) => ({
   },
   note: {
     ...sansSerifyTypography,
-    fontSize: theme.typography.pxToRem(12),
+    fontSize: 14, // fixed font size on tooltip
+    lineHeight: 18.2 / 14,
     "&:last-of-type": {
       marginBottom: 0,
     },
   },
-}))
+}));
 
 const IconWithTooltip = ({
   iconText = "Data notes",
@@ -59,7 +60,7 @@ const IconWithTooltip = ({
   icon = InfoIcon,
   ...props
 }) => {
-  const classes = useStyles()
+  const classes = useStyles();
 
   const tooltipContent = (
     <Box className={classes.tooltip}>
@@ -79,25 +80,30 @@ const IconWithTooltip = ({
         </Typography>
       ))}
     </Box>
-  )
+  );
 
   return (
     <div className={clsx(classes.root, "icon-with-tooltip")}>
-      <Tooltip title={tooltipContent} enterTouchDelay={1}>
+      <Tooltip
+        title={tooltipContent}
+        enterTouchDelay={1}
+        open={true}
+        interactive
+      >
         <ButtonBase className={clsx(classes.iconWrapper, "icon-wrapper")}>
           <img alt="info" src={icon} />
           {iconText}
         </ButtonBase>
       </Tooltip>
     </div>
-  )
-}
+  );
+};
 
 IconWithTooltip.propTypes = {
   notes: PropTypes.array,
   title: PropTypes.string,
   iconText: PropTypes.string,
   icon: PropTypes.object,
-}
+};
 
-export default IconWithTooltip
+export default IconWithTooltip;
