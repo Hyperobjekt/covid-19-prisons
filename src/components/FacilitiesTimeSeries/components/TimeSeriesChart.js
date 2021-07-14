b0d9dc8e3430c2ba1c553fb0e484b08d16899de6
Connor <connor@zero.nyc>
2021-07-14 13:39:27 -0400

    tooltip style updates, truncated labels, add facilitiesById lookup obj

--- src/components/FacilitiesTimeSeries/components/TimeSeriesChart.js
+++ src/components/FacilitiesTimeSeries/components/TimeSeriesChart.js
@@ -17,13 +17,23 @@ import {
 } from "@visx/xychart";
 import useTimeSeriesStore from "../useTimeSeriesStore";
 import shallow from "zustand/shallow";
+import { makeStyles } from "@material-ui/core";
+import clsx from "clsx";
+import { sansSerifyTypography } from "../../../gatsby-theme-hypercore/theme";
 
 const accessors = {
   xAccessor: (d) => new Date(`${d.date}T00:00:00`),
   yAccessor: (d) => Number(d.value),
 };
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    ...sansSerifyTypography,
+  },
+}));
+
 const TimeSeriesChart = () => {
+  const classes = useStyles();
   const facilitiesData = useTimeSeriesData();
 
   const [selectedFacilities, selectedGroup, selectedMetric] =
@@ -40,11 +50,11 @@ const TimeSeriesChart = () => {
 
   const linesData = facilitiesData.map((facilityData) => {
     const lineData = facilityData[accessor];
-    const { id } = facilitiesData;
+    const { id } = facilityData;
     const name = formatFacilityName(facilityData);
     const lastDatum = lineData.reduceRight((accum, datum) => {
-      if (accum) return accum;;
-      if (!!datum.value) return datum;;
+      if (accum) return accum;
+      if (!!datum.value) return datum;
     }, null);
     return { name, id, lineData, lastDatum };
   });
@@ -75,7 +85,24 @@ const TimeSeriesChart = () => {
     }
   };
 
-  console.log("loading:", dataLoading);
+  const maxAnnotationWidth = 19;
+  const facilitiesById = {};
+  selectedFacilities.forEach(({ id, ...facilityData }) => {
+    facilitiesById[id] = facilityData;
+    const labelArr = [
+      facilityData.name,
+      facilityData.state === "*other" ? "" : ", " + facilityData.state,
+    ];
+    facilitiesById[id].truncatedLabel =
+      labelArr.join("").length > maxAnnotationWidth
+        ? labelArr[0]
+            .slice(0, maxAnnotationWidth - labelArr[1].length - 3)
+            .trim() +
+          "..." +
+          labelArr[1]
+        : labelArr.join("");
+  });
+
   return (
     <XYChart
       height={400}
@@ -91,24 +118,23 @@ const TimeSeriesChart = () => {
         // tickLabelProps={() => ({ dx: -10 })}
       />
       <Grid columns={false} numTicks={4} />
-      {linesData.map(({ name, lineData }, i) => (
-        <LineSeries key={name} dataKey={name} data={lineData} {...accessors} />
+      {linesData.map(({ id, lineData }, i) => (
+        <LineSeries key={id} dataKey={id} data={lineData} {...accessors} />
       ))}
       {linesData.map(
-        ({ name, lastDatum }, i) =>
+        ({ id, lastDatum }, i) =>
           lastDatum && (
-            <Annotation dataKey={name} datum={lastDatum} dx={0} dy={0}>
+            <Annotation dataKey={id} datum={lastDatum} dx={0} dy={0} key={i}>
               <AnnotationLabel
-                title={name}
+                title={facilitiesById[id].truncatedLabel}
                 showAnchorLine={false}
                 backgroundFill="#F9FCF8"
-                // titleProps={{ paddingLeft: "35px" }}
-                // fontColor={"#283224"}
                 horizontalAnchor="start"
                 verticalAnchor="middle"
                 width={150}
+                key={0}
               />
-              {/* <AnnotationLabel
+              <AnnotationLabel
                 title={"⬤"}
                 titleProps={{ color: getFacilityColor(i) }}
                 showAnchorLine={false}
@@ -116,25 +142,63 @@ const TimeSeriesChart = () => {
                 fontColor={getFacilityColor(i)}
                 horizontalAnchor="middle"
                 verticalAnchor="middle"
-                // width={10}
-              /> */}
+                key={1}
+              />
             </Annotation>
           )
       )}
       <Tooltip
         snapTooltipToDatumX
         snapTooltipToDatumY
-        showVerticalCrosshair
-        showSeriesGlyphs
+        showDatumGlyph
+        applyPositionStyle
+        style={{
+          backgroundColor: "rgba(32, 32, 32, 0.9)",
+          color: "white",
+          borderRadius: "4px",
+          padding: "16px 20px",
+        }}
         renderTooltip={({ tooltipData, colorScale }) => (
-          <div>
-            <div style={{ color: colorScale(tooltipData.nearestDatum.key) }}>
-              {tooltipData.nearestDatum.key}
+          <div className={clsx(classes.root)}>
+            <span style={{ fontWeight: 700 }}>
+              {facilitiesById[tooltipData.nearestDatum.key].name}
+            </span>
+            <div
+              style={{
+                backgroundImage:
+                  "repeating-linear-gradient(90deg,white,white 1px,transparent 0,transparent 4px)",
+                backgroundPosition: "0 100%",
+                backgroundSize: "100% 1px",
+                backgroundRepeat: "no-repeat",
+                padding: "5px 0 10px",
+              }}
+            >
+              {facilitiesById[tooltipData.nearestDatum.key].state &&
+                facilitiesById[tooltipData.nearestDatum.key].state !==
+                  "*other" && (
+                  <span
+                    style={{
+                      borderRight: "1px solid rgba(255, 255, 255, 0.7)",
+                      paddingRight: "10px",
+                      marginRight: "10px",
+                      fontWeight: 100,
+                    }}
+                  >
+                    {facilitiesById[tooltipData.nearestDatum.key].state}
+                  </span>
+                )}
+              <span style={{ fontWeight: 100 }}>
+                {accessors
+                  .xAccessor(tooltipData.nearestDatum.datum)
+                  .toDateString()}
+              </span>
             </div>
-            {accessors.xAccessor(tooltipData.nearestDatum.datum).toDateString()}
-            <br />
-            {selectedMetric}:
-            {accessors.yAccessor(tooltipData.nearestDatum.datum)}
+            <div style={{ paddingTop: "5px" }}>
+              <span style={{ fontWeight: 700, marginRight: "10px" }}>
+                {accessors.yAccessor(tooltipData.nearestDatum.datum)}
+              </span>
+              <span style={{ fontWeight: 100 }}>{selectedMetric}</span>
+            </div>
           </div>
         )}
       />
