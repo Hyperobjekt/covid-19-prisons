d11517eb0c11568e5ec7159d8d499d2102965761
Connor <connor@zero.nyc>
2021-07-21 18:35:34 -0400

    add useLatestPosts hook

--- src/common/hooks/index.js
+++ src/common/hooks/index.js
@@ -1,7 +1,8 @@
-export { default as useOptionsStore } from "./use-options-store"
-export { default as useFacilitiesData } from "./use-facilities-data"
-export { default as useMappableFacilities } from "./use-mappable-facilities"
-export { default as useHoveredFacilities } from "./use-hovered-facilities"
-export { default as useVaccineData } from "./use-vaccine-data"
-export { default as useDataSelector } from "./use-data-selector"
-export { default as useActiveMetric } from "./use-active-metric"
+export { default as useOptionsStore } from "./use-options-store";
+export { default as useFacilitiesData } from "./use-facilities-data";
+export { default as useMappableFacilities } from "./use-mappable-facilities";
+export { default as useHoveredFacilities } from "./use-hovered-facilities";
+export { default as useVaccineData } from "./use-vaccine-data";
+export { default as useDataSelector } from "./use-data-selector";
+export { default as useActiveMetric } from "./use-active-metric";
+export { default as useLatestPosts } from "./use-latest-posts";
